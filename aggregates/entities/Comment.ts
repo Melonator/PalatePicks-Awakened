cdc75e1e319357f0fab7bebff539f296516c7b81
ylsewlys <129646270+ylsewlys@users.noreply.github.com>
2025-02-19 13:36:52 +0800

    feat: add comment entity class

--- aggregates/entities/Comment.ts
+++ aggregates/entities/Comment.ts
@@ -1,3 +1,33 @@
-class CommentEntity {
+export interface CommentProps {
+  commentId: string;
+  reviewId: string;
+  username: string;
+  commentContent: string;
+  commentDate: Date;
+
+}
+
+export class CommentEntity {
+  public readonly commentId: string;
+  public readonly reviewId: string;
+  public readonly username: string;
+  public content: string;
+  public readonly commentDate: Date;
+
+  constructor(props: CommentProps) {
+    this.commentId = props.commentId;
+    this.reviewId = props.reviewId;
+    this.username = props.username;
+    this.content = props.commentContent;
+    this.commentDate = props.commentDate;
+  }
+
+  public updateCommentContent(newContent: string): void {
+    if(newContent.length < 1){
+      throw new Error("Comment content cannot be empty");
+    }
+    this.content = newContent;
+  }
+
 
 }
