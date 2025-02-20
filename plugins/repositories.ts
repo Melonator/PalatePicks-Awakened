879e5f9bba099f479b4b31d4db77bca0ca88eedd
Melonator <johndayll_arizala@dlsu.edu.ph>
2025-02-20 10:49:59 +0800

    feat: configure plugin

---
+++ plugins/repositories.ts
@@ -0,0 +1,16 @@
+﻿import {RestaurantRepository} from '../repositories/RestaurantRepository'
+import {UserRepository} from "../repositories/UserRepository";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+  const restaurantRepository = new RestaurantRepository(config.public.supabaseUrl,
+    config.public.supabaseAnonKey);
+  const userRepository = new UserRepository(config.public.supabaseUrl,
+    config.public.supabaseAnonKey);
+  return {
+    provide: {
+      restaurantRepository,
+      userRepository,
+    },
+  };
+});
